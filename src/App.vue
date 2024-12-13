9fbe5c87d97a49ef44077b3ae0fa7beee6906cd5
CaduSilva28 <edu420092@gmail.com>
2024-12-12 22:25:42 -0300

    Formulario

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
-  <h1>Guia Clientes</h1>
+  <h3>Cadastro: </h3>
+  <input type="text" placeholder="nome" v-model="nomeField"> <br>
+  <input type="email" placeholder="email" v-model="emailField"> <br>
+  <input type="number" placeholder="idade" v-model="idadeField"> <br>
+  <button @click="cadastrarUsuario">Cadastrar</button>
   <div v-for="(cliente,index) in clientes" :key="cliente.id">
     <h5>{{index + 1}}</h5>
     <ClienteDefault :cliente="cliente"/>
-    <h4>Edição</h4>
-    <input type="text" v-model="cliente.nome">
-    <input type="text" v-model="cliente.email">
   </div>
   <hr>
     <h4>Lista de produtos</h4>
@@ -21,7 +22,9 @@ export default {
   name: 'App',
   data(){
     return {
-      nomeDoCadu: "Carlos Sauro",
+      nomeField: "",
+      emailField: "",
+      idadeField: 0,
       clienteCadu: {
         nome: "Carlos Eduardo",
         email: "marioeduardo@gmail.com",
@@ -45,13 +48,28 @@ export default {
           nome: "Luiza Souza",
           email: "luizasouza@gmail.com",
           idade: 33
-        },
+        }
       ]
     }
   },
   components: {
     ClienteDefault,
     ProdutosDefaultVue
+  },
+  methods: {
+    cadastrarUsuario: function(){
+      this.clientes.push({ 
+        nome: this.nomeField, 
+        email: this.emailField, 
+        idade: this.idadeField,
+        id: Date.now()
+      })
+      
+      //Limpando os campos cadastrados
+      this.nomeField = "";
+      this.emailField = "";
+      this.idadeField = "";
+    }
   }
 }
 </script>
